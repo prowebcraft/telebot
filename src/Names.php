5a1d04009af7148e521e67fe3263ac98b0430a7a
Andrey Mistulov <prowebcraft@gmail.com>
2017-10-05 21:48:36 +0300

    Migration for flex user config

--- src/Names.php
+++ src/Names.php
@@ -13,7 +13,7 @@ namespace Prowebcraft\Telebot;
 trait Names
 {
 
-    private $userNames = [];
+    private $registry = [];
 
     /**
      * Add new user to registry (called automatically on incoming messages)
@@ -30,26 +30,68 @@ trait Names
 
     /**
      * Set User's Name Alias
-     * @param int $userId
+     * @param int $id
      * @param string $userName
      */
-    protected function setUserName($userId, $userName)
+    protected function setUserName($id, $userName)
     {
-        $this->userNames[$userId] = $userName;
-        $this->setConfig("config.names.$userId", $userName);
+        $this->setUserConfig($id, 'name', $userName);
     }
-    
+
+    /**
+     * Retrieve current user config
+     * @param $id
+     * User Id
+     * @param null|string $key
+     * [optional] Config key. If not set - return array with user config
+     * @param mixed $default
+     * Default value for config key
+     * @return mixed
+     */
+    protected function getUserConfig($id, $key = null, $default = null) {
+        if (!isset($this->registry[$id])) {
+            $this->registry[$id] = $this->getConfig("config.names.$id", []);
+            if (is_string($this->registry[$id])) {
+                //Migrate from simple registry
+                $this->registry[$id] = [
+                    'name' => $this->registry[$id]
+                ];
+                $this->setConfig("config.names.$id", $this->registry[$id]);
+            }
+        }
+        if ($key !== null) {
+            if (!isset($this->registry[$id][$key]))
+                return $default;
+            return $this->registry[$id][$key];
+        }
+        return $this->registry[$id];
+    }
+
+    /**
+     * Set config value for user
+     * @param $id
+     * User Id
+     * @param $key
+     * Config key
+     * @param $value
+     * Config value
+     */
+    protected function setUserConfig($id, $key, $value) {
+        $config = $this->getUserConfig($id);
+        $config[$key] = $value;
+        $this->registry[$id] = $config;
+        $this->setConfig("config.names.$id", $config);
+    }
+
     /**
      * Get User Name or Alias
      * @param int $id
+     * @param null $default
      * @return string
      */
     protected function getUserName($id, $default = null)
     {
-        if (!isset($this->userNames[$id])) {
-            $this->userNames[$id] = $this->getConfig("config.names.$id", $default);
-        }
-        return $this->userNames[$id];
+        return $this->getUserConfig($id, 'name', $default);
     }
 
     /**
